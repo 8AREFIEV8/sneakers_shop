f9babc6768d7ddd48de8bad806d9b04228d958d3
Neolist <7759191ai@gmail.com>
2022-01-24 21:58:46 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="wrapper">
-      <header>
-          <div className="headerLeft">
+    <div className="wrapper clear">
+      <header className="d-flex justify-between">
+          <div className="d-flex align-center">
               <img width={40} height={40} src="/img/logo.png.png" alt=""/>
               <div className="headerInfo">
                   <h3>Sneaker World</h3>
@@ -14,11 +14,22 @@ function App() {
           </div>
           <ul className="headerRight">
               <li>
-                  <img width={18} height={18} src="/img/cart.svg" alt=""/>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                       className="bi bi-cart3" viewBox="0 0 16 16">
+                      <path
+                          d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                  </svg>
+
+
                   <span>1205 грн</span>
               </li>
               <li>
-                  <svg />
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                       className="bi bi-person-circle" viewBox="0 0 16 16">
+                      <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                      <path fill-rule="evenodd"
+                            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                  </svg>
               </li>
           </ul>
       </header>
